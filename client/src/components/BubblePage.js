98c2c2b3340315f9f4bc4b0dca1fefcdb1970416
sam hall <sam_h_hall@icloud.com>
2020-08-14 23:02:38 -0500

    it was supposed to be 'headers' -_-

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useHistory } from "react-router-dom";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  const { goBack } = useHistory();
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/colors")
+      .then((res) => {
+        console.log("bubble page color fetch data: ", res.data);
+        setColorList(res.data);
+      })
+      .catch((err) =>
+        console.log("bubble page color fetch err: ", err.message)
+      );
+  }, []);
 
   return (
     <>
+      <button onClick={goBack}>Go Back</button>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,65 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { Formik } from "formik";
+import axios from "axios";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const history = useHistory();
+
+  const handleSubmit = (values) => {
+    // preventDefault();
+    console.log(values);
+    axiosWithAuth()
+      .post("/login", values)
+      .then((res) => {
+        console.log("Success", res);
+        localStorage.setItem("authToken", res.data.payload);
+        history.push("/bubble-page");
+      })
+      .catch((err) => {
+        console.log(err);
+        localStorage.removeItem("authToken");
+      });
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <div>
+        <Formik
+          initialValues={{ username: "", password: "" }}
+          onSubmit={(values, { resetForm }) => {
+            handleSubmit(values);
+            resetForm();
+          }}>
+          {(props) => {
+            const { values, handleSubmit, handleChange } = props;
+            return (
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  name="username"
+                  value={values.username}
+                  placeholder="username"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  name="password"
+                  value={values.password}
+                  placeholder="password"
+                  onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+              </form>
+            );
+          }}
+        </Formik>
+      </div>
     </>
   );
 };
